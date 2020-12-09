042f9c80cec5811053ba8e8a71a2d17aaac30ba3
NandoRunner <nando.az@gmail.com>
2020-12-09 13:20:57 -0300

    Angular update 10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  CURRENT_VERSION: "0.20.25",
+  CURRENT_VERSION: "0.20.27",
   hostingName: "MCoC Library",
   icon: "pulse",
   init_page: "heroes", 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  CURRENT_VERSION: "0.20.25",
+  CURRENT_VERSION: "0.20.27",
   hostingName: "MCoC Library",
   icon: "pulse",
   init_page: "heroes", 
